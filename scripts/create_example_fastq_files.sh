5b68c7dc13894d7266f9032f35fa5f0636438f65
richelbilderbeek <richel@richelbilderbeek.nl>
2023-07-17 08:48:43 +0200

    Simplify

--- scripts/create_example_fastq_files.sh
+++ scripts/create_example_fastq_files.sh
@@ -7,35 +7,48 @@
 #   ./scripts/create_example_fastq_files.sh
 #
 
-echo "@SEQ_ID"                                                       >  reads_s1_t_1.fastq
-echo "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT"  >> reads_s1_t_1.fastq
-echo "+"                                                             >> reads_s1_t_1.fastq
-echo "\!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65" >> reads_s1_t_1.fastq
+cat > reads_s1_t_1.fastq << EOF
+@SEQ_ID
+GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
++
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+EOF
 
-echo "@SEQ_ID"                                                       >  reads_s1_t_2.fastq
-echo "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT"  >> reads_s1_t_2.fastq
-echo "+"                                                             >> reads_s1_t_2.fastq
-echo "\!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65" >> reads_s1_t_2.fastq
+cat > reads_s1_t_2.fastq << EOF
+@SEQ_ID
+GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
++
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+EOF
 
-echo "@SEQ_ID"                                                       >  reads_s1_n_1.fastq
-echo "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT"  >> reads_s1_n_1.fastq
-echo "+"                                                             >> reads_s1_n_1.fastq
-echo "\!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65" >> reads_s1_n_1.fastq
+cat > reads_s1_n_1.fastq << EOF
+@SEQ_ID
+GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
++
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+EOF
 
-echo "@SEQ_ID"                                                       >  reads_s1_n_2.fastq
-echo "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT"  >> reads_s1_n_2.fastq
-echo "+"                                                             >> reads_s1_n_2.fastq
-echo "\!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65" >> reads_s1_n_2.fastq
+cat > reads_s1_n_2.fastq << EOF
+@SEQ_ID
+GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
++
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+EOF
 
-echo "@SEQ_ID"                                                       >  reads_s1_r_1.fastq
-echo "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT"  >> reads_s1_r_1.fastq
-echo "+"                                                             >> reads_s1_r_1.fastq
-echo "\!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65" >> reads_s1_r_1.fastq
 
-echo "@SEQ_ID"                                                       >  reads_s1_r_2.fastq
-echo "GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT"  >> reads_s1_r_2.fastq
-echo "+"                                                             >> reads_s1_r_2.fastq
-echo "\!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65" >> reads_s1_r_2.fastq
+cat > reads_s1_r_1.fastq << EOF
+@SEQ_ID
+GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
++
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+EOF
+
+cat > reads_s1_r_2.fastq << EOF
+@SEQ_ID
+GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
++
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+EOF
 
 gzip reads_s1_t_1.fastq
 gzip reads_s1_t_2.fastq
