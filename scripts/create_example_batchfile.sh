9765ea86bd26267fd96de869860a4a670fb5738b
richelbilderbeek <richel@richelbilderbeek.nl>
2023-07-17 08:44:09 +0200

    Simplified script

--- scripts/create_example_batchfile.sh
+++ scripts/create_example_batchfile.sh
@@ -7,9 +7,11 @@
 #   ./scripts/create_example_batchfile.sh
 #
 
-echo "sampleName,reads1,reads2,sampleType,HLAfile,sex"                        >  example_batchfile.csv
-echo "sample1,reads_s1_t_1.fastq.gz,reads_s1_t_2.fastq.gz,tumor_DNA,,female"  >> example_batchfile.csv
-echo "sample1,reads_s1_n_1.fastq.gz,reads_s1_n_2.fastq.gz,normal_DNA,,female" >> example_batchfile.csv
-echo "sample1,reads_s1_r_1.fastq.gz,reads_s1_r_2.fastq.gz,tumor_RNA,,female"  >> example_batchfile.csv
+cat > example_batchfile.csv << EOF
+sampleName,reads1,reads2,sampleType,HLAfile,sex
+sample1,reads_s1_t_1.fastq.gz,reads_s1_t_2.fastq.gz,tumor_DNA,,female
+sample1,reads_s1_n_1.fastq.gz,reads_s1_n_2.fastq.gz,normal_DNA,,female
+sample1,reads_s1_r_1.fastq.gz,reads_s1_r_2.fastq.gz,tumor_RNA,,female
+EOF
 
 mv example_batchfile.csv nextNEOpi/
